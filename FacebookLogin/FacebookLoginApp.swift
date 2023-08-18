b6f6e87e92923778cc8927a0c95c462daf9e5466
YagoCardoso <y.maksuell@gmail.com>
2023-08-18 12:03:57 -0300

    Initial Commit

---
+++ FacebookLogin/FacebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookLoginApp.swift
+//  FacebookLogin
+//
+//  Created by User on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FacebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
